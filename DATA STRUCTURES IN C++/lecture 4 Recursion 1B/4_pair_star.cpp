510efb53d9b86e52ca09f3a81f2a04ecb792faae
TUSHAR MAURYA <tusharmaurya002@gmail.com>
2022-08-03 00:04:15 +0530

    one more

--- DATA STRUCTURES IN C++/lecture 4 Recursion 1B/4_pair_star.cpp
+++ DATA STRUCTURES IN C++/lecture 4 Recursion 1B/4_pair_star.cpp
@@ -25,89 +25,81 @@ a*a*a*a
 
 
 
-/*
 
-#include<string.h>
-#include <bits/stdc.h++>
-void pairStarhelp(char input[],int start)
-{
-    if(input[start]=='\0')
-    {
-        return ;
-    }
-    
-    else if(input[start]!='\0')
-    {
-        if(input[start]==input[start-1])
-        {
-            for(int i=strlen(input);i>=start;i--)
-            {
-                input[i+1]=input[i];
-            }
-            input[start]='*';
-            
-        }
-    pairStarhelp(input,start+1);
-    }
-}
-void pairStar(char input[]) 
-{
-    pairStarhelp(input,1);
-}
-
-int main() 
-{
-   char input[100];
-   cin.getline(input, 100);
-   pairStar(input);
-   cout << input << endl;
-}
+// #include <string.h>
+// #include <bits/stdc++.h>
+// #include <iostream>
+// using namespace std;
 
 
-*/
+// void star_shift(char input[] , int start , int end)
+// {
+//     if( start == end )
+//     {
+//         input[end+1] = input[end];
+//         input[end] = '*';
+//         return ;
+//     }
+//     else
+//     {
+//         input[end+1] = input[end];
+//         star_shift( input , start , --end);
+//     }
+// }
 
-#include <string.h>
-#include <bits/stdc++.h>
-#include <iostream>
-using namespace std;
+// void helper(char input[] , int len )
+// {
+//     int change_len=strlen(input);
+//     if( len == 0 )
+//     {
+//         return ;
+//     }
+//     else
+//     {
+//         if(input[len] == input[len-1])
+//         {
+//             star_shift( input , len , change_len);
+//         }
+//         helper(input , --len);
+//     }
+// }
+
+// void pairStar(char input[]) 
+// {
+//     int len=strlen(input);
+//     helper( input , len );
+// }
+
+
 
 
 void star_shift(char input[] , int start , int end)
-{
-    if( start == end )
-    {
-        input[end+1] = input[end];
-        input[end] = '*';
-        return ;
+{   
+    if((start+1)==end){
+        input[start+1]='*';
+        return;
     }
-    else
-    {
-        input[end+1] = input[end];
-        star_shift( input , start , --end);
-    }
+    input[end]=input[end-1];
+    star_shift( input , start , --end);
 }
 
-void helper(char input[] , int len )
+void helper(char input[] ,int start, int end )
 {
-    int change_len=strlen(input);
-    if( len == 0 )
-    {
-        return ;
+    
+    if(start==end){
+        return;
     }
-    else
-    {
-        if(input[len] == input[len-1])
-        {
-            star_shift( input , len , change_len);
-        }
-        helper(input , --len);
+    if(input[start]==input[start+1]){
+        end=end+1;
+        star_shift(input ,start ,end);
     }
+    helper(input ,++start ,end);
 }
 
 void pairStar(char input[]) 
 {
     int len=strlen(input);
-    helper( input , len );
+    helper( input ,0 , len );
 }
 
 
