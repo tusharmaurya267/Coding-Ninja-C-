fd35e8652a128b86a6c8d158ec9c9ad7607994a8
TUSHAR MAURYA <tusharmaurya002@gmail.com>
2022-08-02 23:41:36 +0530

    one more

--- DATA STRUCTURES IN C++/lecture 4 Recursion 1B/1_replace_pi.cpp
+++ DATA STRUCTURES IN C++/lecture 4 Recursion 1B/1_replace_pi.cpp
@@ -27,41 +27,51 @@ Constraints:-
 #include <iostream>
 using namespace std;
 
-/*
-void replacePiHelper(char str[], int start)
-{
-
-    if (str[start] == '\0' || str[start + 1] == '\0') 
-    {
-        return;
-    }
- 
-
-    replacePiHelper(str, start + 1);
-
-    if (str[start] == 'p' && str[start + 1] == 'i') 
-    {
-
-        for (int i = strlen(str); i >= start + 2; i--) 
-        {
-            str[i + 2] = str[i];
-        }
-
-        str[start] = '3';
-        str[start + 1] = '.';
-        str[start + 2] = '1';
-        str[start + 3] = '4';
-    }
-}
-
-void replacePi(char str[])
-{
-    replacePiHelper(str, 0);
-}
-*/
 
 
 #include <bits/stdc++.h>
+
+// void shift_insert(char input[] , int start , int end)
+// {
+//     if(end==start)
+//     {
+//         input[start]='3';
+//         input[start+1]='.';
+//         input[start+2]='1';
+//         input[start+3]='4';
+//         return;
+//     }
+//     else
+//     {
+//         input [end+2] = input [end];
+//         shift_insert(input ,  start , --end);
+//     }
+// }
+
+// void helper(char input[] , int len)
+// {
+//     int change_len =strlen(input);
+//     if(len<=0 )
+//     {
+//         return ;
+//     }
+//     else
+//     {
+//         if( input[len] == 'i' && input[len-1] == 'p' )
+//         {
+//             shift_insert( input , len-1 ,change_len);
+//             len--;
+//         }
+//         helper(input , --len);
+//     }
+// }
+
+// void replacePi(char input[]) 
+// {
+//     int len=strlen(input);
+//     helper(input , len-1);
+// }
+
 
 void shift_insert(char input[] , int start , int end)
 {
@@ -73,37 +83,30 @@ void shift_insert(char input[] , int start , int end)
         input[start+3]='4';
         return;
     }
-    else
-    {
-        input [end+2] = input [end];
-        shift_insert(input ,  start , --end);
-    }
+    input [end] = input [end-2];
+    shift_insert(input ,  start , end-1);
 }
 
-void helper(char input[] , int len)
+void helper(char input[] , int start, int end)
 {
-    int change_len =strlen(input);
-    if(len<=0 )
-    {
-        return ;
+    if(start==end ){
+        return;
     }
-    else
-    {
-    //     if( input[len] == 'i' && input[len-1] == 'p' )
-    //     {
-    //         shift_insert( input , len-1 ,change_len);
-    //         len--;
-    //     }
-    //     helper(input , --len);
-    // }
+    
+    if(input[start]=='p' && input[start+1]=='i'){
+        end=end+2;
+        
+        shift_insert(input,start,end);
+    }
+    helper(input,++start,end);
 }
+
+
 
 void replacePi(char input[]) 
 {
-    int len=strlen(input);
-    helper(input , len-1);
+    helper(input, 0, strlen(input));
 }
-
 
 
 int main() {
