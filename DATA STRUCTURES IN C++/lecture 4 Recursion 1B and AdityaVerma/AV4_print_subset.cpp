8eea8f17d9ec3ce341fbfeee84753d98abb3caaf
TUSHAR MAURYA <tusharmaurya002@gmail.com>
2022-08-09 03:14:26 +0530

    some more

---
+++ DATA STRUCTURES IN C++/lecture 4 Recursion 1B and AdityaVerma/AV4_print_subset.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void print_subset(string input,string output){
+    if(input.length()==0){
+        cout << output << endl;
+        return;
+    }
+    string output1=output+input[0];
+    string output2=output;
+    input.erase(input.begin()+0);
+    print_subset(input,output1);
+    print_subset(input,output2);
+    return;
+}
+
+int main(){
+    string input;
+    cin >> input;
+    string output;
+    print_subset(input,output);
+    return 0;
+}
